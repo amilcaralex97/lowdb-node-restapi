8ffbfaa5c6065e4639c89a208192f5fff341dfb2
Amilcar Sanchez <amilcaralex97@gmail.com>
2020-03-08 23:06:25 -0600

    get and create task implemented

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -3,4 +3,15 @@ const {
 } = require('express')
 const router = Router()
 
+const {
+    getTasks,
+    createTask
+} = require('../controllers/TasksController')
+
+router.get('/tasks', getTasks)
+router.get('/tasks/:id', )
+router.post('/tasks', createTask)
+router.put('/tasks/:id', )
+router.delete('/tasks/:id', )
+
 module.exports = router
